8fe8f6393147eee0167f25e44f83c4a811cb0059
vincentxwd <kirai.wendong@gmail.com>
2021-06-15 17:02:23 +0800

    update

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
+# GLaDOS机场自动签到服务器版
+因为github的Action不知道什么时候会干掉自动签到脚本（比如米游社的签到已经被干掉了），所以写了这个服务器版的。
+
+机场地址：https://glados.rocks/
+
+**注册、填写邀请码可以获得5天免费使用：JBRNX-L3XXC-Q1BHD-I8HNT**
+
+### 获取方式
+1. git clone git@github.com:VincentXWD/glados_checkin_server.git
+2. docker pull kirailol/glados_checkin:latest
+
 ### Usage
 0. 自行获取COOKIE
 1. docker build -t glados_checkin . -f ./Dockerfile
 2. docker run -dit --restart unless-stopped --name=glados_checkin -d glados_checkin "\<Your COOKIE\>"
+
+
+### COOKIE获取
+1. 打开 chrome, 登录到 glados 控制台, 找到签到按钮, 按 F12, 拿到 cookie
+![img](fig/cookie.png)
+2. 复制COOKIE到Usage中的步骤2对应位置。
--- app/glados_checkin.py
+++ app/glados_checkin.py
@@ -5,29 +5,23 @@ import os
 result = b'success\n'
 # url
 url = "https://glados.rocks/api/user/checkin"
-# cookie
-cookie = ''
-
-payload = "{\"token\":\"glados_network\"}"
-headers = {
-    'authority': 'glados.rocks',
-    'accept': 'application/json, text/plain, */*',
-    'dnt': '1',
-    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.80 Safari/537.36',
-    'content-type': 'application/json;charset=UTF-8',
-    'origin': 'https://glados.rocks',
-    'sec-fetch-site': 'same-origin',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-dest': 'empty',
-    'referer': 'https://glados.rocks/console/checkin',
-    'accept-language': 'zh-CN,zh;q=0.9',
-    'cookie': cookie
-}
-
 
 def main(COOKIE: str):
-    global cookie
-    cookie = COOKIE
+    payload = "{\"token\":\"glados_network\"}"
+    headers = {
+        'authority': 'glados.rocks',
+        'accept': 'application/json, text/plain, */*',
+        'dnt': '1',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.80 Safari/537.36',
+        'content-type': 'application/json;charset=UTF-8',
+        'origin': 'https://glados.rocks',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-dest': 'empty',
+        'referer': 'https://glados.rocks/console/checkin',
+        'accept-language': 'zh-CN,zh;q=0.9',
+        'cookie': COOKIE
+    }
     logger = logging.getLogger()
     response = requests.request("POST", url, headers=headers, data=payload)
     result = response.text
